d4dc40ad6acae2a783a041b24cba6fda2ce1fa08
particle4dev <particle4dev@gmail.com>
2015-06-11 09:25:26 +0700

    update

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,12 +3,15 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var concat      = require('gulp-concat');
+var sourcemaps  = require('gulp-sourcemaps');
 var config      = require('../config').sass;
 // http://alfanso.com/concatinating-all-css-files-into-a-single-css-file-using-gulp-js/
 
 gulp.task('sass', function () {
     gulp.src(config.src)
+    .pipe(sourcemaps.init())
     .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.output))
     .pipe(concat('main.css'))
     .pipe(gulp.dest(config.dest));
